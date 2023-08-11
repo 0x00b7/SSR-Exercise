8946c2b13882908c7393658bdd7bdc4f08143ba4
0x00b7 <0x00b7@gmail.com>
2023-08-11 15:14:15 +0200

    @

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
       <FormHandle />
       <div className="grid gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {res.map((DATA: Product) => (
-          <div id={DATA.ID?.toString()} className="flex flex-col items-center w-[200px] p-2 border-2 border-gray-200 rounded-lg shadow-md">
+          <div key={DATA.ID} className="flex flex-col items-center w-[200px] p-2 border-2 border-gray-200 rounded-lg shadow-md">
             <FormDelete ID={DATA.ID!} />
             <h1 className="text-2xl font-bold">{DATA.ITEM}</h1>
             <p className="text-xl font-bold">{DATA.PRICE}€</p>
